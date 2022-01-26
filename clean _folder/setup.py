67b384d955a23da0bb1e44a2e8f6e89d1da025e7
Shakal1985 <shakal1985@i.ua>
2022-01-26 21:51:16 +0200

    lll

--- clean _folder/setup.py
+++ clean _folder/setup.py
@@ -7,7 +7,7 @@ setup(
           'console_scripts' : ['clean-folder=clean_folder.clean:clean'],
           },
     description='Sorting trash into cathegories',
-    url='https://github.com/Shakal1985/Shakal1985/tree/main/clean%20folder',
+    url='https://github.com/Shakal1985/Clean_folder_script_DZ7/tree/main/clean%20_folder',
     author='Dmytro Chernyshev',
     author_email='shakal1985@i.ua',
     license='MIT',
